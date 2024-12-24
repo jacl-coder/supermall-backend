e937c51cc332f12137fc913a617dd95bad1a75d9
LaiXinpeng <laix1024@gmail.com>
2024-12-24 21:08:37 +0800

    Resolve merge conflicts

---
+++ src/main/java/com/supermall/backend/SupermallBackendApplication.java
@@ -0,0 +1,13 @@
+package com.supermall.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermallBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermallBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/supermall/backend/SupermallBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermall.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermallBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
